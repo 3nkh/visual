9e26824a72511e2df1df512b120007d171c00efd
enkh <enkhtsetsegbayrmagnai@gmail.com>
2020-02-25 00:52:51 +0800

    first commit

---
+++ Enkh/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Enkh
+{
+    class Class1
+        
+    {
+        private string n;
+        //field    
+        //property
+        public string m;
+        //field iin utgiig heveldeg utga onoodog method
+        public void hevleh()
+        {
+            n = Console.ReadLine();
+            Console.Write("n:");
+            Console.WriteLine(n);
+        }
+        public void hevleh1()
+        {
+            m = Console.ReadLine();
+            Console.Write("m:");
+            Console.WriteLine(m);
+        }
+    }   
+}
---
+++ Enkh/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace Enkh
+{
+    namespace lab1
+    {
+        class Task1
+        {
+            private static object enkh;
+              
+            static void Main(string[] args)
+            {
+                string a;
+                a=Console.ReadLine();
+                Console.WriteLine(a);
+                
+                Class1 g=new Class1();
+                g.hevleh();
+                g.hevleh1();
+                
+            }
+        }
+    }
+}
